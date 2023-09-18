e9f7f718920ba6ebd71314c42e12cf04caaf63ff
felix-hauger <felix.hauger@laplateforme.io>
2023-09-18 15:16:51 +0200

    Finish jour1 job04: use application.yml & @Value annotation to inject values into my controller

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${greeting.message}")
+    private String message;
+
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World";
+        return "Hello World " + this.message;
     }
 }
