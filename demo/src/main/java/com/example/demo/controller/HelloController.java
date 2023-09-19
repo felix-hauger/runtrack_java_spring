c884f719e1d6ed76e9dc84486b4c57edc2d78e8c
felix-hauger <felix.hauger@laplateforme.io>
2023-09-19 10:21:35 +0200

    Finish jour1 job06: add DevTools dependency

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,10 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +14,7 @@ public class HelloController {
     private String profilMessage;
 
     @GetMapping("/")
-    public String sayHello(String profil) {
+    public String sayHello() {
         return "Hello World " + this.message + "<br/>" + this.profilMessage;
     }
 }
