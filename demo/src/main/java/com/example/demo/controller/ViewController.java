734526694632f9c7b3273bf1c38d3b39895ed3d6
felix-hauger <felix.hauger@laplateforme.io>
2023-09-21 14:45:53 +0200

    Finish jour2 job03: iterate on list of objects with thymeleaf

--- demo/src/main/java/com/example/demo/controller/ViewController.java
+++ demo/src/main/java/com/example/demo/controller/ViewController.java
@@ -1,18 +1,34 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Message;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+
 @Controller
 public class ViewController {
-    private String message;
+    private String welcomeMessage;
+
+    private ArrayList<Message> messageList = new ArrayList<Message>();
 
      @GetMapping("/view")
     public String showView(Model model) {
-         this.message = "voilivoilou";
+         this.welcomeMessage = "voilivoilou";
 
-         model.addAttribute("message", this.message);
+         Message message1 = new Message("toto", "Salut");
+         Message message2 = new Message("caca", "tu");
+         Message message3 = new Message("hmm", "vas");
+         Message message4 = new Message("oki", "bien");
+
+         this.messageList.add(message1);
+         this.messageList.add(message2);
+         this.messageList.add(message3);
+         this.messageList.add(message4);
+
+         model.addAttribute("message", this.welcomeMessage);
+         model.addAttribute("messageList", this.messageList);
 
          return "view";
      }
---
+++ demo/src/main/java/com/example/demo/model/Message.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+public class Message {
+    private String author;
+
+    private String content;
+
+    public Message(String author, String content) {
+        this.author = author;
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
