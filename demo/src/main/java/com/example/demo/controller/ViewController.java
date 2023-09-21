5b8b62c60938cd1783570a6c008f2a1a8a8ce255
felix-hauger <felix.hauger@laplateforme.io>
2023-09-21 15:46:49 +0200

    Finish jour3 job04: add new message using form, thymeleaf, annotations & redirections

--- demo/src/main/java/com/example/demo/controller/ViewController.java
+++ demo/src/main/java/com/example/demo/controller/ViewController.java
@@ -4,6 +4,8 @@ import com.example.demo.model.Message;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 
@@ -17,19 +19,27 @@ public class ViewController {
     public String showView(Model model) {
          this.welcomeMessage = "voilivoilou";
 
-         Message message1 = new Message("toto", "Salut");
-         Message message2 = new Message("caca", "tu");
-         Message message3 = new Message("hmm", "vas");
-         Message message4 = new Message("oki", "bien");
+//         Message message1 = new Message("toto", "Salut");
+//         Message message2 = new Message("caca", "tu");
+//         Message message3 = new Message("hmm", "vas");
+//         Message message4 = new Message("oki", "bien");
+//
+//         this.messageList.add(message1);
+//         this.messageList.add(message2);
+//         this.messageList.add(message3);
+//         this.messageList.add(message4);
 
-         this.messageList.add(message1);
-         this.messageList.add(message2);
-         this.messageList.add(message3);
-         this.messageList.add(message4);
-
-         model.addAttribute("message", this.welcomeMessage);
+         model.addAttribute("welcomeMessage", this.welcomeMessage);
          model.addAttribute("messageList", this.messageList);
+         model.addAttribute("newMessage", new Message());
 
          return "view";
+     }
+
+     @PostMapping("/view")
+    public String saveMessage(@ModelAttribute Message newMessage) {
+         this.messageList.add(newMessage);
+
+         return "redirect:view";
      }
 }
